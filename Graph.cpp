761e1576fad9213f55d5b490499c952dc8d6a45b
OmerApt <156093828+OmerApt@users.noreply.github.com>
2024-05-18 17:40:04 +0300

    overloading arithmetic operators

--- Graph.cpp
+++ Graph.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 namespace ariel
 {
+
     void Graph::loadGraph(vector<vector<int>> g)
     {
         unsigned int nodes = g.size();
@@ -20,11 +21,13 @@ namespace ariel
         }
         if (this->actual_graph != nullptr)
         {
-            free(this->actual_graph);
+            // free(this->actual_graph);
+            delete[] this->actual_graph;
         }
         this->num_edges = 0;
         this->nodes_num = nodes;
-        this->actual_graph = (int *)malloc(nodes * nodes * sizeof(int));
+        // this->actual_graph = (int *)malloc(nodes * nodes * sizeof(int));
+        this->actual_graph = new int[nodes * nodes];
         if (this->actual_graph != NULL)
         {
             for (unsigned int row = 0; row < nodes; row++)
@@ -39,32 +42,249 @@ namespace ariel
         }
     }
 
+    void Graph::loadGraph(Graph g)
+    {
+        unsigned int nodes = g.get_num_of_nodes();
+        if (this->actual_graph != nullptr)
+        {
+            // free(this->actual_graph);
+            delete[] this->actual_graph;
+        }
+        this->num_edges = 0;
+        this->nodes_num = nodes;
+        // this->actual_graph = (int *)malloc(nodes * nodes * sizeof(int));
+        this->actual_graph = new int[nodes * nodes];
+        if (this->actual_graph != NULL)
+        {
+            for (unsigned int row = 0; row < nodes; row++)
+            {
+                for (unsigned int col = 0; col < nodes; col++)
+                {
+                    int edge = g.getedge(row, col);
+                    this->num_edges += (edge != 0) ? 1 : 0;
+                    this->setedge(row, col, edge);
+                }
+            }
+        }
+    }
+
     void Graph::setedge(unsigned int a, unsigned int b, int value)
     {
         // need to check if parameters in range for safer code
-        if (a >= nodes_num || b >= nodes_num) {
-                throw std::out_of_range("Node index out of range.");
-            }
+        if (a >= nodes_num || b >= nodes_num)
+        {
+            throw std::out_of_range("Node index out of range.");
+        }
         this->actual_graph[this->nodes_num * a + b] = value;
     }
 
-    int Graph::getedge(unsigned int row, unsigned int collumn)
+    int Graph::getedge(unsigned int row, unsigned int collumn) const
     {
         // need to check if parameters in range for safer code
-        if (row >= nodes_num || col >= nodes_num) {
-                throw std::out_of_range("Node index out of range.");
-            }
+        if (row >= nodes_num || collumn >= nodes_num)
+        {
+            throw std::out_of_range("Node index out of range.");
+        }
         return this->actual_graph[this->nodes_num * row + collumn];
     }
 
-    void Graph::printGraph()
+    Graph &Graph::operator+=(const Graph &g2)
+    {
+        if (this->nodes_num != g2.nodes_num)
+        {
+            throw out_of_range("The + operation can only be used for graphs with the same number of nodes");
+        }
+        uint number_of_nodes = this->nodes_num;
+        this->num_edges = 0;
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                int new_val = this->getedge(row, col) + g2.getedge(row, col);
+                this->setedge(row, col, new_val);
+                if (new_val != 0)
+                    this->num_edges++;
+            }
+        }
+
+        return *this;
+    }
+
+    Graph &Graph::operator-=(const Graph &g2)
+    {
+        if (this->nodes_num != g2.nodes_num)
+        {
+            throw out_of_range("The + operation can only be used for graphs with the same number of nodes");
+        }
+        uint number_of_nodes = this->nodes_num;
+        this->num_edges = 0;
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                int new_val = this->getedge(row, col) - g2.getedge(row, col);
+                this->setedge(row, col, new_val);
+                if (new_val != 0)
+                    this->num_edges++;
+            }
+        }
+
+        return *this;
+    }
+
+    Graph Graph::operator*(int scalar)
+    {
+        uint number_of_nodes = this->nodes_num;
+        Graph g_res{number_of_nodes};
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                int new_val = this->getedge(row, col) * scalar;
+                g_res.setedge(row, col, new_val);
+                if (new_val != 0)
+                    g_res.num_edges++;
+            }
+        }
+
+        return g_res;
+    }
+
+    Graph &Graph::operator++(int dummy_flag)
+    {
+        uint number_of_nodes = this->nodes_num;
+        this->num_edges = 0;
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                if (this->getedge(row, col) != 0)
+                {
+                    int new_val = this->getedge(row, col) + 1;
+                    this->setedge(row, col, new_val);
+                    if (new_val != 0)
+                        this->num_edges++;
+                }
+            }
+        }
+
+        return *this;
+    }
+
+    Graph &Graph::operator--(int dummy_flag)
+    {
+        uint number_of_nodes = this->nodes_num;
+        this->num_edges = 0;
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                if (this->getedge(row, col) != 0)
+                {
+                    int new_val = this->getedge(row, col) - 1;
+                    this->setedge(row, col, new_val);
+                    if (new_val != 0)
+                        this->num_edges++;
+                }
+            }
+        }
+
+        return *this;
+    }
+
+    void Graph::printGraph() const
     {
         cout << "Graph with " << this->nodes_num << " vertices and " << this->num_edges << " edges" << std::endl;
+    }
+
+    // creates an empty graph with param nodenum number of nodes
+    Graph::Graph(unsigned int nodenum)
+    {
+        this->actual_graph = new int[nodenum * nodenum];
+        this->nodes_num = nodenum;
+        this->num_edges = 0;
+        this->graph_loaded = true;
     }
 
     Graph::~Graph()
     {
         if (this->graph_loaded == true)
-            free(this->actual_graph);
+        {
+            // free(this->actual_graph);
+            delete[] this->actual_graph;
+        }
+    }
+    Graph operator+(const Graph &g1, const Graph &g2)
+    {
+        if (g1.nodes_num != g2.nodes_num)
+        {
+            throw out_of_range("The + operation can only be used for graphs with the same number of nodes");
+        }
+        uint number_of_nodes = g1.nodes_num;
+        Graph g_res(number_of_nodes);
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                int new_val = g1.getedge(row, col) + g2.getedge(row, col);
+                g_res.setedge(row, col, new_val);
+                if (new_val != 0)
+                    g_res.num_edges++;
+            }
+        }
+
+        return g_res;
+    }
+    Graph operator-(const Graph &g1, const Graph &g2)
+    {
+        if (g1.nodes_num != g2.nodes_num)
+        {
+            throw out_of_range("The + operation can only be used for graphs with the same number of nodes");
+        }
+        uint number_of_nodes = g1.nodes_num;
+        Graph g_res(number_of_nodes);
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                int new_val = g1.getedge(row, col) - g2.getedge(row, col);
+                g_res.setedge(row, col, new_val);
+                if (new_val != 0)
+                    g_res.num_edges++;
+            }
+        }
+
+        return g_res;
+    }
+    Graph operator+(const Graph &g1)
+    {
+        uint number_of_nodes = g1.nodes_num;
+        Graph g_res(number_of_nodes);
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                int new_val = g1.getedge(row, col);
+                g_res.setedge(row, col, +new_val);
+            }
+        }
+
+        return g_res;
+    }
+    Graph operator-(const Graph &g1)
+    {
+        uint number_of_nodes = g1.nodes_num;
+        Graph g_res(number_of_nodes);
+        g_res.num_edges = g1.num_edges;
+        for (uint row = 0; row < number_of_nodes; row++)
+        {
+            for (uint col = 0; col < number_of_nodes; col++)
+            {
+                int new_val = g1.getedge(row, col);
+                g_res.setedge(row, col, -new_val);
+            }
+        }
+
+        return g_res;
     }
 }
--- Graph.hpp
+++ Graph.hpp
@@ -1,4 +1,4 @@
-//206766677 Omer.Apter@msmail.ariel.ac.il
+// 206766677 Omer.Apter@msmail.ariel.ac.il
 #include <vector>
 #pragma once
 
@@ -13,7 +13,7 @@ namespace ariel
         unsigned int num_edges;
         // use 1d array to represent matrice
         int *actual_graph;
-        //if have loaded graph
+        // if have loaded graph
         bool graph_loaded;
         // for initilazing graph
         void setedge(unsigned int a, unsigned int b, int value);
@@ -24,17 +24,31 @@ namespace ariel
             this->actual_graph = nullptr;
             this->nodes_num = 0;
             this->num_edges = 0;
-            this->graph_loaded = 0;
+            this->graph_loaded = false;
         };
+        Graph(unsigned int nodenum);
         ~Graph();
         void loadGraph(std::vector<std::vector<int>> g);
-        void printGraph();
+        // copy load
+        void loadGraph(Graph g);
+        void printGraph() const;
         // return the edge from a to b if exists else return 0
-        int getedge(unsigned int row, unsigned int collumn);
-        unsigned int get_num_of_nodes()
+        int getedge(unsigned int row, unsigned int collumn) const;
+        unsigned int get_num_of_nodes() const
         {
             return this->nodes_num;
         }
+        // binary + -
+        friend Graph operator+(const Graph &g1, const Graph &g2);
+        friend Graph operator-(const Graph &g1, const Graph &g2);
+        Graph &operator+=(const Graph &g2);
+        Graph &operator-=(const Graph &g2);
+        Graph operator*(int scalar);
+        Graph &operator++(int dummy_flag);
+         Graph &operator--(int dummy_flag);
+        // unary + -
+        friend Graph operator+(const Graph &g1);
+        friend Graph operator-(const Graph &g1);
     };
 
 } // namespace ariel
